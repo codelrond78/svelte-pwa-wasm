3906b55620b214b99a973f38d07fafbfa4657323
codelrond <codelrond@protonmail.com>
2022-05-29 17:14:09 +0200

    added wasm call

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import Counter from '../lib/Counter.svelte'
   import Go from '../lib/Go.svelte'
+  import init from '../lib/example.wasm'
+
+  init().then((exports) => {
+    let r = exports.add(3, 4)
+    console.log("result of add(3,4) is", r)
+  })
 </script>
 
 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
